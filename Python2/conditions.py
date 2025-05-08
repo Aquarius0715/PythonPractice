80c106ff8623529402576c5f527a1599a483c748
ji1wxs <ji1wxs@outlook.jp>
2025-05-08 10:37:39 +0900

    comlplete python basic2

---
+++ Python2/conditions.py
@@ -0,0 +1,9 @@
+num = 10
+
+if num > 10:
+    print("numは10以上")
+elif num == 10:
+    print("numは10")
+else:
+    print("numは10以下")
+
---
+++ Python2/loops.py
@@ -0,0 +1,17 @@
+for i in range(5, 10, 1):
+    print(i)
+
+print()
+    
+list = ['a', 'b', 'c', 'd']
+for i in list:
+    print(i)
+
+n = 0
+while n < 10:
+    print(n)
+    n += 1
+
+while True:
+    print("無限ループ")
+    
